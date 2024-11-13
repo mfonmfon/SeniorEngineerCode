fa028403dad10de380760cd64daa969bf8431943
mfonmfon <mfonm579@gmail.com>
2024-11-13 09:52:22 +0100

    done with the models add relations to them

---
+++ src/Chapter16/Example7.java
@@ -0,0 +1,24 @@
+package Chapter16;
+
+import transaction.Transaction;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Example7 {
+
+    public static void main(String[] args) {
+
+//        List<Integer> numbers = List.of(1,2,3,4,5,6,6,5,4,44,55);
+//
+//        numbers.stream()
+//                .filter(value -> value > 0 )
+//                .map((element) -> {
+//                    return new Transaction(
+//                            "Mfon",
+//                            "Paul")
+//                })
+
+    }
+}
